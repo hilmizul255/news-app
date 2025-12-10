d40cc36d3510d0e29f41a663301e05f5b06921ef
hilmizul255 <hilmizul255@gmail.com>
2025-12-11 00:10:51 +0800

    Proper Error Handling

--- src/api/newsApi.jsx
+++ src/api/newsApi.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchNews = async (searchResult) => {
     try {
         const query = searchResult || 'latest';
-        const response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=9adb73fa559b4fb2b1db74bfe0e34f40`);
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
         return response.data.articles;
     } catch (error) {
         throw error;
